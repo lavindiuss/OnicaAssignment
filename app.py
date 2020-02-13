61d7ac1c078be075cbb22832806edae0ea24dd74
lavindiuss <gergesfikry1@gmail.com>
2020-02-13 22:02:49 +0200

    --format code with black--

--- app.py
+++ app.py
@@ -1,53 +1,60 @@
-import os 
-import sys 
-from orm import * 
+import os
+import sys
+from orm import *
 
 LoadDatabaseBooks()
 # object contains all actions user can perform
 MainActionObject = {
     "View all books": "1",
-    "Add a book":"2",
-    "Edit a book":"3",
-    "Search for a book":"4",
-    "Save and exit":"5"
+    "Add a book": "2",
+    "Edit a book": "3",
+    "Search for a book": "4",
+    "Save and exit": "5",
 }
 
-#start-up function that show count of books in db
+# start-up function that show count of books in db
 def StartUp():
     while True:
-        os.system('cls' if os.name == 'nt' else 'clear')
-        for key,value in MainActionObject.items():
-            print(str(value)+")"+key)
-        user_input = input('Choose [1-5]:') 
-        if user_input in [str(i) for i in range(1,5)]:
+        os.system("cls" if os.name == "nt" else "clear")
+        for key, value in MainActionObject.items():
+            print(str(value) + ")" + key)
+        user_input = input("Choose [1-5]:")
+        if user_input in [str(i) for i in range(1, 5)]:
             return
         do_something(user_input)
 
-#this function calls an orm function debending on action
-class Strategy():
+
+# this function calls an orm function debending on action
+class Strategy:
     def do_logic(self):
         pass
+
 
 class ListStrategy(Strategy):
     def do_logic(self):
         UserBooksView()
+
 
 class CreateBookStrategy(Strategy):
     def do_logic(self):
         title = input("Title:")
         author = input("Author:")
         description = input("Description:")
-        SaveNewBookToDatabase(title,author,description)
+        SaveNewBookToDatabase(title, author, description)
+
 
 class EditBookStrategy(Strategy):
     def do_logic(self):
-        book_id = input("Enter the book ID of the book you want to edit; to return press <Enter>.\nBook ID:")
+        book_id = input(
+            "Enter the book ID of the book you want to edit; to return press <Enter>.\nBook ID:"
+        )
         if not book_id:
             return
         title = input("Title:")
         author = input("Author:")
         description = input("Description:")
-        EditBook(book_id,title,author,description)
+        EditBook(book_id, title, author, description)
+
 
 class SearchForBookStrategy(Strategy):
     def do_logic(self):
@@ -62,16 +69,15 @@ def do_something(action):
         1: ListStrategy,
         2: CreateBookStrategy,
         3: EditBookStrategy,
-        4: SearchForBookStrategy
+        4: SearchForBookStrategy,
     }
     startegy = funct_logics.get(action)
     startegy().do_logic()
 
 
-if __name__ == '__main__':
-    
+if __name__ == "__main__":
+
     print("== BOOK MANAGER ==")
     StartUp()
 
     # arr = list(map(int, input().rstrip().split()))
-
--- orm.py
+++ orm.py
@@ -1,33 +1,37 @@
 import sqlite3 as sq
 
 
-
 # this function create a database with all required tables and pass if its already exist
 """
 database for : database connection 
 database_cursor for : the cursor that controls the structure that enables traversal over the records in the database
 """
-database=sq.connect('Books.db')
-database_cursor=database.cursor()
+database = sq.connect("Books.db")
+database_cursor = database.cursor()
+
 
 def DatabaseCreation():
-    database_cursor.execute('CREATE TABLE IF NOT EXISTS Book(id INTEGER PRIMARY KEY AUTOINCREMENT,title VARCHAR(200),author VARCHAR(255),description VARCHAR(500))')
+    database_cursor.execute(
+        "CREATE TABLE IF NOT EXISTS Book(id INTEGER PRIMARY KEY AUTOINCREMENT,title VARCHAR(200),author VARCHAR(255),description VARCHAR(500))"
+    )
+
+
 DatabaseCreation()
 
 
-
-# this function loads and return a list of db books 
+# this function loads and return a list of db books
 """
 books_list for : list of books that shows the output of books query 
 """
+
+
 def LoadDatabaseBooks():
     try:
-        books_list = database_cursor.execute('SELECT * FROM Book').fetchall()
-        print('--'+str(len(books_list))+' BOOKS LOADED INTO THE LIBRARY--\n')
+        books_list = database_cursor.execute("SELECT * FROM Book").fetchall()
+        print("--" + str(len(books_list)) + " BOOKS LOADED INTO THE LIBRARY--\n")
         return books_list
     except Exception as e:
-        print (str(e))    
-
+        print(str(e))
 
 
 # this function list the ids and the titles of each book , and also returns a book details if it got a book id
@@ -35,34 +39,46 @@ def LoadDatabaseBooks():
 list_of_ids_and_titles for : list contains all ids and titles of books 
 book_details for : tuple contains particular book data 
 """
+
+
 def UserBooksView(book_id=None):
-    list_of_ids_and_titles = database_cursor.execute('SELECT id,title FROM Book').fetchall()
+    list_of_ids_and_titles = database_cursor.execute(
+        "SELECT id,title FROM Book"
+    ).fetchall()
     if book_id != None:
-        book_details = database_cursor.execute('SELECT * FROM Book WHERE id = {}'.format(book_id)).fetchone()
-        return book_details   
+        book_details = database_cursor.execute(
+            "SELECT * FROM Book WHERE id = {}".format(book_id)
+        ).fetchone()
+        return book_details
     return list_of_ids_and_titles
 
 
-
-#this function takes the id of a book and partial update its attributes
+# this function takes the id of a book and partial update its attributes
 """
 data_to_edit for : list contains all book data we can edit
 """
-def EditBook(book_id=None,title=None,author=None,description=None):
-    data_to_edit = [title,author,description,book_id]
-    database_cursor.execute('UPDATE Book SET title= ?,author= ?,description= ? WHERE id= ?',data_to_edit)
+
+
+def EditBook(book_id=None, title=None, author=None, description=None):
+    data_to_edit = [title, author, description, book_id]
+    database_cursor.execute(
+        "UPDATE Book SET title= ?,author= ?,description= ? WHERE id= ?", data_to_edit
+    )
     database.commit()
-    print('--Book edited successfully--')
-    
+    print("--Book edited successfully--")
 
 
-# this function takes new book required attributes and save it to database 
+# this function takes new book required attributes and save it to database
 """
 data_to_save for : list of data that contains new book details
 """
-def SaveNewBookToDatabase(title=None,author=None,description=None):
-    data_to_save = [title,author,description]
-    database_cursor.execute('INSERT INTO Book(title,author,description) VALUES (?,?,?)',data_to_save)
+
+
+def SaveNewBookToDatabase(title=None, author=None, description=None):
+    data_to_save = [title, author, description]
+    database_cursor.execute(
+        "INSERT INTO Book(title,author,description) VALUES (?,?,?)", data_to_save
+    )
     database.commit()
     print("--Book saved successfully--")
 
@@ -71,6 +87,10 @@ def SaveNewBookToDatabase(title=None,author=None,description=None):
 """
 result for : attribute contains search resutl
 """
+
+
 def SearchForBook(keyword=None):
-    result = database_cursor.execute("SELECT * FROM Book WHERE title like '%{}%'".format(keyword)).fetchall()
+    result = database_cursor.execute(
+        "SELECT * FROM Book WHERE title like '%{}%'".format(keyword)
+    ).fetchall()
     return result
